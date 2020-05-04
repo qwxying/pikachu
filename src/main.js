d358dc0caa1460caae2823ee1bc7d8c3fabb798e
qwxying <qwxying@gmail.com>
2020-05-04 17:43:51 +0800

    加了一些音效和动画

--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
 const $nose = $(".nose");
 const $tongue = $(".tongue");
-
+const $ball = $(".ball");
 const $sound1 = $("#sound1").get(0);
 const $sound2 = $("#sound2").get(0);
+const $sound3 = $("#sound3").get(0);
 
+$sound2.play();
+setTimeout(()=>{$sound1.play();},3000)
 $nose.on("mouseover", () => {
   $sound1.play();
 });
 $tongue.on("mouseover", () => {
   $sound2.play();
+});
+
+$ball.on("click", () => {
+  $sound3.play();
 });
