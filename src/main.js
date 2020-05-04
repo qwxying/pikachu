02004d84d0a3e3e88267fa341d38fa3c20d08021
qwxying <qwxying@gmail.com>
2020-05-04 18:25:15 +0800

    一些微调

--- src/main.js
+++ src/main.js
@@ -5,8 +5,14 @@ const $sound1 = $("#sound1").get(0);
 const $sound2 = $("#sound2").get(0);
 const $sound3 = $("#sound3").get(0);
 
-$sound2.play();
-setTimeout(()=>{$sound1.play();},3000)
+// $sound2.play();
+setTimeout(() => {}, 1000);
+setTimeout(() => {
+  $sound2.play();
+}, 1000);
+setTimeout(() => {
+  $sound1.play();
+}, 3000);
 $nose.on("mouseover", () => {
   $sound1.play();
 });
